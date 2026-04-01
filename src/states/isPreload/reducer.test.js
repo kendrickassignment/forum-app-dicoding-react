98c2abd5a60a9b6724eb69ce77a166e5f157ec8f
Kendrick Filbert <letscook6565@gmail.com>
2026-04-01 18:57:03 +0700

    fix: remove failing test

--- src/states/isPreload/reducer.test.js
+++ src/states/isPreload/reducer.test.js
@@ -4,7 +4,6 @@
  * - isPreloadReducer function
  *   - should return the initial state when given by unknown action
  *   - should return isPreload value when given by SET_IS_PRELOAD action
- *   - should fail on purpose
  */
 
 import { describe, it, expect } from 'vitest';
@@ -31,9 +30,5 @@ describe('isPreloadReducer function', () => {
     const nextState = isPreloadReducer(initialState, action);
 
     expect(nextState).toBe(false);
-  });
-
-  it('should fail on purpose', () => {
-    expect(true).toBe(false);
   });
 });
